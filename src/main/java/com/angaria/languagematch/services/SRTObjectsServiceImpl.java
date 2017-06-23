61f3403316334b729fc34590cd0139a368fc8f51
Alex Bretet <alex.bretet@gmail.com>
2017-06-23 06:51:09 +0100

    Prevent multiple file processing

--- src/main/java/com/angaria/languagematch/services/SRTObjectsServiceImpl.java
+++ src/main/java/com/angaria/languagematch/services/SRTObjectsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.angaria.languagematch.services;
 
-import com.angaria.languagematch.components.Workflow;
 import com.angaria.languagematch.entities.SRTObject;
 import com.angaria.languagematch.repositories.SRTObjectRepository;
 import com.angaria.languagematch.wrappers.SRTObjects;
@@ -10,8 +9,8 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Transactional
 @Service
@@ -26,7 +25,18 @@ public class SRTObjectsServiceImpl implements SRTObjectsService {
     public SRTObjects persist(SRTObjects srtObjects) {
         logger.log(Level.INFO, "Persisting subtitle objects");
 
-        List<SRTObject> srtObjectList = srtObjectRepository.save(srtObjects.getSrtObjects());
+        List<SRTObject> srtObjectList = new ArrayList<>();
+        for (SRTObject SRTObject : srtObjects.getSrtObjects()){
+            SRTObject object = srtObjectRepository.findOne(SRTObject.getFileName());
+            if(object == null){
+                srtObjectRepository.save(SRTObject);
+                srtObjectList.add(SRTObject);
+            }
+            else{
+                logger.log(Level.INFO, SRTObject.getFileName() + " already persisted... will not be reattempted!");
+            }
+        }
+
         srtObjects.setSrtObjects(srtObjectList);
         return srtObjects;
     }
